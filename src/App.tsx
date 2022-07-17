2d37c0a35cb288742ec4939d18666e916decc60b
Jorge Sanz <jorge@jorgesanz.net>
2022-07-17 17:52:57 +0200

    using scss with pico, changed favicon, and other improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,50 @@
 import React, { useState } from "react";
 
+const LABEL_COPY = "Copy to the clipboard";
+const LABEL_COPIED = "Done!";
+
 function App() {
-  const [mimimi,setMimimi] = useState("");
+  const [mimimi, setMimimi] = useState("");
+  const [clipboard, setClipboard] = useState(LABEL_COPY);
 
   const onClickHandle = () => {
     navigator.clipboard.writeText(mimimi);
-    console.log(`${mimimi} escrito en el portapapeles`);
-  }
+    setClipboard(LABEL_COPIED);
+  };
 
-  const onChangeHandle = (event: React.FormEvent<HTMLTextAreaElement> ) => {
+  const onChangeHandle = (event: React.FormEvent<HTMLTextAreaElement>) => {
     const preMimimi = event.currentTarget.value;
     const mimimiValue = preMimimi
-    .replace(/[aeiou]/g,'i')
-    .replace(/[áéíóú]/g,'í');
+      .replace(/[aeiou]/g, "i")
+      .replace(/[AEIOU]/g, "I")
+      .replace(/[áéíóú]/g, "í")
+      .replace(/[ÁÉÍÓÚ]/g, "Í");
     setMimimi(mimimiValue);
-  }
+    if (clipboard == LABEL_COPIED) {
+      setClipboard(LABEL_COPY);
+    }
+  };
 
   return (
-    <main className="container">
-      <header>
-        <h1>mimimi</h1>
-      </header>
-      <main>
-        <p>Use this form to replace all vowels in your text by i.</p>
-        <form>
-          <div className="grid">
-            <label htmlFor="entry">
-              The text you want to mimimify
-              <textarea onChange={onChangeHandle} id="entry" defaultValue="Paste your text here"></textarea>
-            </label>
-            <label htmlFor="output">
-              Thiri yii gi:
-              <textarea readOnly={true} id="output" value={mimimi}>
-              </textarea>
-            </label>
-            <button
-              type="button"
-              onClick={onClickHandle}
-            >
-              Copy to clipboard
-            </button>
-          </div>
-        </form>
-      </main>
-    </main>
+    <form>
+      <div className="grid">
+        <label htmlFor="entry">
+          The text you want to mimimify
+          <textarea
+            onChange={onChangeHandle}
+            id="entry"
+            defaultValue="Paste your text here"
+          ></textarea>
+        </label>
+        <label htmlFor="output">
+          Thiri yii gi:
+          <textarea readOnly={true} id="output" value={mimimi}></textarea>
+        </label>
+        <button type="button" onClick={onClickHandle}>
+          {clipboard}
+        </button>
+      </div>
+    </form>
   );
 }
 
